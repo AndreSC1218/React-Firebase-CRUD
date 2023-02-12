e8510070d615719ac8bfe4fad07df1ff4a56ca16
André Salazar <71437593+AndreSC1218@users.noreply.github.com>
2023-02-12 15:14:05 +0100

    ADD: Read Database
    ADD: Read Database

--- react-firebase-crud/src/components/LinkForm.js
+++ react-firebase-crud/src/components/LinkForm.js
@@ -16,7 +16,6 @@ export const LinkForm = (props) =>{
     //maneja el cambio del input
     const handleInputChange = e =>{
         const {name, value} = e.target;
-        console.log(name, value);
         setValues({...values, [name]: value})
     }
 
@@ -25,7 +24,11 @@ export const LinkForm = (props) =>{
     const handleSubmit = e =>{
         //para que no refrescar la pagina
         e.preventDefault();
+        //se guardan los datos
         props.addOrEditLink(values);
+        //
+        setValues({...initialStateValues})
+
     }
 
     return (
@@ -34,17 +37,17 @@ export const LinkForm = (props) =>{
                 <div className="input-group-text bg-light">
                     <i class="material-icons">insert_link</i>
                 </div>
-                <input onChange={handleInputChange} type="text" className="form-control" placeholder="https://someurl.com" name="url"/>
+                <input onChange={handleInputChange} type="text" className="form-control" placeholder="https://someurl.com" name="url" value={values.url}/>
             </div>
             <div className="form-group input-group p-1">
                 <div className="input-group-text bg-light">
                 <i class="material-icons">create</i>
                 </div>
-                <input onChange={handleInputChange} type="text" className="form-control" name="name" placeholder="website name"/>                
+                <input onChange={handleInputChange} type="text" className="form-control" name="name" placeholder="website name" value={values.name}/>                
             </div>
             <div>
                 <div className="form-group p-1">
-                    <textarea onChange={handleInputChange} name="description" rows="3" className="form-control" placeholder="write a description"/>
+                    <textarea onChange={handleInputChange} name="description" rows="3" className="form-control" placeholder="write a description" value={values.description}/>
                 </div>
             </div>
             <button className="btn btn-primary btn-block p-1">
--- react-firebase-crud/src/components/Links.js
+++ react-firebase-crud/src/components/Links.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { LinkForm } from './LinkForm';
+//useeffect para cuando cargue el componente hacer la peticion de los links
+import { useEffect } from "react";
+import { useState } from "react";
 
 //importamos db firebase
 import {db} from "../firebase"
 //necesitamos esto para acceder a las colleciones
 import { collection, addDoc } from "firebase/firestore";
+//traemos getDocs
+import { getDocs } from "firebase/firestore";
+//onSnapshot para escuchar cambios en tiempo real
+import { onSnapshot } from "firebase/firestore";
 
 export const Links = () =>{
+
+    const [links, setLinks] = useState([]);
 
     //añadir un link
     const addOrEditLink = async (linkObject) =>{
@@ -14,8 +23,37 @@ export const Links = () =>{
         console.log("new link added");
     }
 
+    //obtener links
+    const getLinks = async () => {
+        onSnapshot(collection(db, "links"), (querySnapshot) => {
+        const docs = [];
+          querySnapshot.forEach((doc) => {
+            docs.push({...doc.data(), id:doc.id})
+          });
+          setLinks(docs);
+        });
+      };
+
+    //
+    useEffect(() => {
+        getLinks();
+    }, []);
+
     return <div>
-        <LinkForm addOrEditLink = {addOrEditLink}/>
-        <h1>Links</h1>
+        <div className="col-md-4 p-2">
+            <LinkForm addOrEditLink = {addOrEditLink}/>
+        </div>
+        <div className="col-md-8 p-2">
+            {links.map(link => (
+                <div className="card mb-1">
+                    <div className="card-body">
+                    <h4>{link.name}</h4>
+                    <h4>{link.description}</h4>
+                    <a href={link.url} target="_blank">Go to website</a>
+                </div>
+                </div>
+            ))}
+        </div>
+
     </div>;
 };
